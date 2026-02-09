1e3ef931dfefc4cdf3b7e34fe1ca9146c3a6fffd
Sandeep Konda <kondasandeep56@gmail.com>
2026-02-09 17:25:48 +0530

    Update ecs.tf

--- terrafoam/ecs.tf
+++ terrafoam/ecs.tf
@@ -1,3 +1,22 @@
 resource "aws_ecs_cluster" "blog_cluster" {
   name = "blog-cluster"
 }
+resource "aws_ecs_task_definition" "blog_task" {
+  family                   = "personalblog"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "personalblog"
+      image = "<ECR_IMAGE_URL>"
+      portMappings = [
+        {
+          containerPort = 80
+        }
+      ]
+    }
+  ])
+}
